0f6d60bc457d7ff3ccdeaceb4b9252dde78b66c7
Jose Suarez <jose95sp@outlook.com>
2018-05-30 18:37:18 -0500

    template index ready

--- src/main/java/com/josesp/students_register/controller/IndexController.java
+++ src/main/java/com/josesp/students_register/controller/IndexController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class StudentController {
+public class IndexController {
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String showList(){
-        return "list";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(){
+        return "index";
     }
 
 }
